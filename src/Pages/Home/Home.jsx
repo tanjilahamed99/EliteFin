088df9907fb08f86683ae70f1a89e9f43c2b601a
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-30 17:06:56 +0600

    navbar complete

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div className="container mx-auto">
+           <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
